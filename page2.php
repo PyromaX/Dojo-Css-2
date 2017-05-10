15934524515bd3139079c2978bbef5dc815877bd
PyromaX <jeux.pyromaxx@gmail.com>
2017-05-10 16:32:37 +0200

    first

---
+++ page2.php
@@ -0,0 +1,18 @@
+<?php include ('header.php'); ?>
+<section class="one">
+    <h3>TAGGED *GOATS*</h3>
+    <hr class="hrp2">
+    <div>
+        <img src="./img/sun.jpg">
+        <p class="p2">It's been hard to keep this one a secret, but excited to finally announce that as of September 2015, all Little Seed products will be produced using 100% renewable energy - we're going solar! We're long been commited to having a positive impact on the environment, so after years of haping and planning, we're proud to finally achieve this goal. Our hearts are happy</p>
+        <ul>
+            <li>&#149; 2015-8</li>
+            <li>&#149; FARMINGHOWTO</li>
+            <li>&#149; GOATS</li>
+        </ul>
+        <form>
+            <input type="submit" value="READ MORE &#8594">
+        </form>
+    </div>
+</section>
+<?php include ('footer.php'); ?>
---
+++ page3.php
@@ -0,0 +1,7 @@
+<?php include ('header.php'); ?>
+<section class="one">
+    <h1>OUR STORY & MISSION</h1>
+    <img src="./img/feild.jpg">
+    <p class="p3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias amet animi aperiam autem delectus deleniti dolorem exercitationem expedita explicabo fugiat illum ipsam ipsum quaerat quas rem rerum soluta, voluptatibus?</p>
+</section>
+<?php include ('footer.php'); ?>
